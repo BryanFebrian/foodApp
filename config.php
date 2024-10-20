356809b7994ef26f5463ee86c5f3b5e8077bcf57
David-21110419 <211110419@students.mikroskil.ac.id>
2024-10-20 12:57:36 +0700

    penambahan note

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 
 $conn = mysqli_connect('127.0.0.1:8111', 'root', '', 'cart_db');
 
+//$conn = mysqli_connect('localhost', 'root', '', 'cart_db',3307); //ini punya david
+
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
