e6265bbdb552cbf799cdb234b8e3098b0d08edb2
David-21110419 <211110419@students.mikroskil.ac.id>
2024-10-20 13:12:50 +0700

    penambahan login, register, logout user, fix admin page

--- config.php
+++ config.php
@@ -1,11 +1,23 @@
 <?php
 
 $conn = mysqli_connect('127.0.0.1:8111', 'root', '', 'cart_db');
-
-//$conn = mysqli_connect('localhost', 'root', '', 'cart_db',3307); //ini punya david
+//$conn = mysqli_connect('localhost', 'root', '', 'cart_db',3307); //ini punya david 
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+// Koneksi ke database users_db
+$conn_users = mysqli_connect('127.0.0.1:8111', 'root', '', 'user_db');
+//$conn_users = mysqli_connect('localhost', 'root', '', 'user_db',3307); //ini punya david
+
+// Cek koneksi ke users_db
+if (!$conn_users) {
+    die("Connection failed to user_db: " . mysqli_connect_error());
+}
 ?>
+
+
+
+
