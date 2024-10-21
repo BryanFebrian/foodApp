d6b73555c68d681daf1a16ae555175168f227e0d
GilbertChenz <pocococo111@gmail.com>
2024-10-21 07:06:42 +0700

    Menambahkan About Us Page

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 
 $conn = mysqli_connect('127.0.0.1:8111', 'root', '', 'cart_db');
-//$conn = mysqli_connect('localhost', 'root', '', 'cart_db',3307); //ini punya david 
 
 // Check connection
 if (!$conn) {
@@ -10,7 +9,6 @@ if (!$conn) {
 
 // Koneksi ke database users_db
 $conn_users = mysqli_connect('127.0.0.1:8111', 'root', '', 'user_db');
-//$conn_users = mysqli_connect('localhost', 'root', '', 'user_db',3307); //ini punya david
 
 // Cek koneksi ke users_db
 if (!$conn_users) {
